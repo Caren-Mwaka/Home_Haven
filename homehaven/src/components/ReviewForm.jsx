adb23d169e33f16fe175b1a1191138871e229a96
ianngure <kayngure57@gmail.com>
2024-07-13 21:08:10 +0300

    feat:added initial values

--- homehaven/src/components/ReviewForm.jsx
+++ homehaven/src/components/ReviewForm.jsx
@@ -2,3 +2,11 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup'
 import './Reviews.css';
+
+const ReviewForm = () => {
+    const initialValues = {
+      user_id: '',
+      room_id: '',
+      rating: '',
+      comment: ''
+    };

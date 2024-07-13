f5b0dc15011d5b24174237242074f2ee18df37a7
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-07-13 20:34:52 +0300

    added user model

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy_serializer import SerializerMixin
-from  sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.associationproxy import association_proxy
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from config import db

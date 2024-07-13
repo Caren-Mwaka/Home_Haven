59a271c0ae9f5ffd4107e4314f99fe40f5a62a9c
victor <wahinyavictor818@gmail.com>
2024-07-13 21:07:54 +0300

    add relationship

--- server/models.py
+++ server/models.py
@@ -29,17 +29,20 @@ class User(db.Model):
 class Room(db.Model):
     __tablename__ = 'rooms'
 
-     id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     room_number = db.Column(db.String(100), nullable=False, unique=True)
     type = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Numeric(10, 2), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-bookings = db.relationship('Booking', back_populates='room', cascade='all, delete-orphan')
+
+    bookings = db.relationship('Booking', back_populates='room', cascade='all, delete-orphan')
     reviews = db.relationship('Review', back_populates='room', cascade='all, delete-orphan')
 
- def __repr__(self):
+
+    def __repr__(self):
         return f'<Room {self.room_number}>'
+
 
 class Booking(db.Model):
     __tablename__ = 'bookings'
@@ -50,3 +53,10 @@ class Booking(db.Model):
     check_in_date = db.Column(db.Date, nullable=False)
     check_out_date = db.Column(db.Date, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    user = db.relationship('User', back_populates='bookings')
+    room = db.relationship('Room', back_populates='bookings')
+
+    def __repr__(self):
+        return f'<Booking {self.id}>'
+

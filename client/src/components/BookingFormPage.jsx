cb7a58330362d3f715fb5c21d1f88038c9b2028f
John Austin Ngode <austinngode23@gmail.com>
2024-07-13 22:53:38 +0300

    Feat:Renders the Booking form

--- client/src/components/BookingFormPage.jsx
+++ client/src/components/BookingFormPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import BookingForm from './Bookingform';
 
 function BookingFormPage() {
+  const { roomId } = useParams();
 
   const handleBookingSubmit = (values) => {
     console.log('Booking Data:', values);
@@ -9,6 +12,7 @@ function BookingFormPage() {
   return (
     <div>
       <h2>Book Room {roomId}</h2>
+      <BookingForm onSubmit={handleBookingSubmit} roomId={roomId} />
     </div>
   );
 }

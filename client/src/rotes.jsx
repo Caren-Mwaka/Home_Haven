ad083324b606aa5dce812d744040f8e2e36af649
Sharon <Sharonaquilla28@gmail.com>
2024-07-13 21:40:41 +0300

    feat:adds routing

---
+++ client/src/rotes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import LoginSignupPage from "./Components/LoginSignup/LoginSignupPage";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+   
+  },
+  {
+    path:'/LoginSignup',
+    element:<LoginSignupPage/>
+  },
+]);
+
+export default router;

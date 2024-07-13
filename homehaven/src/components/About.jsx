b7a1d60bc5e2f364cc44937659e12731091908f2
ianngure <kayngure57@gmail.com>
2024-07-13 20:46:56 +0300

    feat: added the content section

---
+++ homehaven/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './About.css';
+
+
+
+const About = () => {
+    return (
+        <div className="about-page">
+      <div className="about-container">
+        <h1>About Our Hotel</h1>
+        <p>Welcome to our luxurious hotel, where comfort meets elegance. Home Haven stands as the epitome of opulence, where luxury meets unparalleled comfort. Nestled in a prime location, this five-star sanctuary boasts exquisitely designed suites adorned with sumptuous fabrics, bespoke furnishings, and breathtaking views. Each room is a haven of tranquility, featuring state-of-the-art amenities and personalized services that cater to the most discerning travelers. The hotel's grandiose architecture and meticulously landscaped gardens offer an atmosphere of regal splendor. Guests indulge in world-class dining experiences, where gourmet cuisine is artfully presented, and the finest wines flow. At Hotel Haven, every moment is crafted to offer an unforgettable experience of refined elegance and supreme comfort.</p>
+        
+        <h2>Our Mission</h2>
+        <p>To provide exceptional hospitality and create lasting memories for our guests through personalized service and attention to detail.</p>
+        

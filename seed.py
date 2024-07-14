4c202575aa6cef166a195c5d4ecce477dc337d28
Brian <brian.m.muigai@gmail.com>
2024-07-14 11:02:49 +0300

    feat:added room data

--- seed.py
+++ seed.py
@@ -6,3 +6,8 @@ users_data = [
     {'name': 'James Bond', 'email': 'jamesbond@gmail.com', 'password': 'James5566'},
     {'name': 'Jane Quartz', 'email': 'jquartz13@gmail.com', 'password': 'jQuarTz13'},
 ]
+
+rooms_data = [
+    {'number': '420', 'type': 'single', 'price': 10000, 'availability': True},
+    {'number': '690', 'type': 'double', 'price': 15000, 'availability': True},
+]

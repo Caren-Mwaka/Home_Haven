096c9627d1efdcd8c4a992e93dd0dcc5dde9ccbf
Brian <brian.m.muigai@gmail.com>
2024-07-14 11:02:03 +0300

    feat:user data added

--- seed.py
+++ seed.py
@@ -1,3 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import User, Room, Booking, Review 
+
+users_data = [
+    {'name': 'James Bond', 'email': 'jamesbond@gmail.com', 'password': 'James5566'},
+    {'name': 'Jane Quartz', 'email': 'jquartz13@gmail.com', 'password': 'jQuarTz13'},
+]
